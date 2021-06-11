aa0b44a30eb2d4e124ca45210af6315373bf0210
aytennihal <80786330+aytennihal@users.noreply.github.com>
2021-06-11 15:50:29 +0300

    started hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Developing hello feature");
+        System.out.println("Github Repo is already published");
+        System.out.println("Doing more work");
+
     }
 }
